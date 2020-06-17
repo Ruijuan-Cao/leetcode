6b701ca09821d1d3d9e055c806b2f403564cfcc7
crj-gmail <caoruijuan13@gmail.com>
2020-06-17 19:16:17 +0800

    1 two num

--- twonum/src/main.rs
+++ twonum/src/main.rs
@@ -10,27 +10,32 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
+use std::collections::HashMap;
 
-pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    for i in 0..nums.len() {
-        for j in i + 1..nums.len() {
-            if nums[i] + nums[j] == target {
-                return vec![i as i32, j as i32];
+pub struct Solution {}
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for i in 0..nums.len() {
+            for j in i + 1..nums.len() {
+                if nums[i] + nums[j] == target {
+                    return vec![i as i32, j as i32];
+                }
             }
         }
+        vec![]
     }
-    vec![]
+    pub fn two_sum2(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        vec![]
+    }
 }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-pub fn two_sum2(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    vec![]
-}
-
-fn main() {
-    println!("Hello, world!");
-
-    let nums: Vec<i32> = vec![2, 7, 11, 15];
-    let target: i32 = 9;
-    let result = two_sum(nums, target);
-    println!("result:{:?}", result);
+    #[test]
+    fn test_1() {
+        assert_eq!(vec![0, 1], Solution::two_sum(vec![2, 7, 11, 15], 9));
+        assert_eq!(vec![1, 2], Solution::two_sum(vec![3, 2, 4], 6));
+    }
 }
