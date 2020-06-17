f186e812c211e04c8af54ee46b32a6fe7abfa94c
crj-gmail <caoruijuan13@gmail.com>
2020-06-17 19:17:10 +0800

    1 two num

--- twonum/target/debug/deps/twonum-46ddbd5b6d4f64cd.d
+++
@@ -1,5 +0,0 @@
-/Users/crj/Documents/code/rustlab/leetcode/twonum/target/debug/deps/twonum-46ddbd5b6d4f64cd: src/main.rs
-
-/Users/crj/Documents/code/rustlab/leetcode/twonum/target/debug/deps/twonum-46ddbd5b6d4f64cd.d: src/main.rs
-
-src/main.rs:
--- twonum/target/debug/twonum.d
+++
@@ -1,1 +0,0 @@
-/Users/crj/Documents/code/rustlab/leetcode/twonum/target/debug/twonum.dSYM: /Users/crj/Documents/code/rustlab/leetcode/twonum/src/main.rs
