c04c3c5628bd34543b2b7a956769fbfb522e8e44
crj-gmail <caoruijuan13@gmail.com>
2020-06-17 19:15:29 +0800

    get 10000 primes

--- addtwo/src/main.rs
+++ addtwo/src/main.rs
@@ -23,6 +23,7 @@ impl ListNode {
         ListNode { next: None, val }
     }
 }
+/*
 impl Solution {
     pub fn add_two_numbers(
         l1: Option<Box<ListNode>>,
@@ -31,7 +32,68 @@ impl Solution {
 
     }
 }
+*/
+
+/* c code - the first 10000 prime
+#include <stdio.h>
+#define MAXNUM 10000
+
+void func()
+{
+    register int i, j;
+    int cnt;
+    int flag;
+    int findvalue[MAXNUM] ={2};
+
+    for(cnt = 1, i = 3; cnt < MAXNUM; i += 2)
+    {
+        for(j = 0; flag = ((findvalue[j] * findvalue[j]) <= i); j ++) {
+            if(!(i % findvalue[j]))break;
+        }
+        if(!flag)findvalue[cnt ++] = i;
+    }
+
+    printf("%d\n", findvalue[MAXNUM - 1]);
+}
+
+int main()
+{
+    func();
+    return 0;
+}
+*/
+
+const MAXNUM: usize = 10000;
+//get the first 10000 prime
+pub fn get_10000_primes() {
+    let mut flag = false;
+    let mut value = vec![2; MAXNUM];
+    let mut count = 1;
+    let mut i = 3;
+    loop {
+        let mut j = 0;
+        loop {
+            flag = (value[j] * value[j] > i);
+            if (i % value[j] == 0) || flag {
+                break;
+            }
+            j += 1;
+        }
+
+        if flag {
+            value[count] = i;
+
+            count += 1;
+        }
+        if count == MAXNUM {
+            break;
+        }
+        i += 2;
+    }
+    println!("primes:{:?}", value);
+}
 
 fn main() {
     println!("Hello, world!");
+    get_10000_primes();
 }
