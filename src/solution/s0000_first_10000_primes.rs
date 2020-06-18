e2c710185c49017939b9713704634d39aec1b5ba
crj-gmail <caoruijuan13@gmail.com>
2020-06-18 17:27:06 +0800

    leetcode

---
+++ src/solution/s0000_first_10000_primes.rs
@@ -0,0 +1,38 @@
+pub const MAXNUM: usize = 10000;
+
+pub fn first_10000_primes() -> u32 {
+    let mut primes = vec![2; MAXNUM];
+    let mut count = 1;
+
+    let mut flag = false;
+    let mut i = 3;
+    loop {
+        let mut j = 0;
+        loop {
+            flag = (primes[j] * primes[j] > i);
+            if flag || i % primes[j] == 0 {
+                break;
+            }
+            j += 1;
+        }
+        if flag {
+            primes[count] = i;
+            count += 1;
+        }
+        if count == MAXNUM {
+            return primes[MAXNUM - 1];
+        }
+        i += 2;
+    }
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_0() {
+        assert_eq!(104729, first_10000_primes());
+    }
+}
---
+++ src/solution/s0005_longest_palindromic_substring.rs
@@ -0,0 +1,48 @@
+/**
+ * [5] Longest Palindromic Substring
+ *
+ * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+ *
+ * Example 1:
+ *
+ *
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: "cbbd"
+ * Output: "bb"
+ *
+ *
+ */
+pub struct Solution {}
+
+// problem: https://leetcode.com/problems/longest-palindromic-substring/
+// discuss: https://leetcode.com/problems/longest-palindromic-substring/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+impl Solution {
+    pub fn longest_palindrome(s: String) -> String {}
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_5() {
+        assert_eq!(Solution::longest_palindrome("aaaaa".to_owned()), "aaaaa");
+        assert_eq!(Solution::longest_palindrome("babab".to_owned()), "babab");
+        assert_eq!(Solution::longest_palindrome("babcd".to_owned()), "bab");
+        assert_eq!(Solution::longest_palindrome("cbbd".to_owned()), "bb");
+        assert_eq!(Solution::longest_palindrome("bb".to_owned()), "bb");
+        assert_eq!(Solution::longest_palindrome("".to_owned()), "");
+    }
+}
