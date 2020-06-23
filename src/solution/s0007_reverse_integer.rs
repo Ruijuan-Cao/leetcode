8322cec6622c87f98d2fdb5aee93dcd379fc1040
crj-gmail <caoruijuan13@gmail.com>
2020-06-23 17:28:15 +0800

    add s00007

--- src/solution/s0007_reverse_integer.rs
+++ src/solution/s0007_reverse_integer.rs
@@ -35,7 +35,25 @@ pub struct Solution {}
 
 // submission codes start here
 impl Solution {
-    pub fn reverse(x: i32) -> i32 {}
+    pub fn reverse(x: i32) -> i32 {
+        let mut result = 0;
+        let mut num = x;
+        while num != 0 {
+            let div = num % 10;
+            if (result > std::i32::MAX / 10
+                || (result == std::i32::MAX / 10 && div > 7)
+                || result < std::i32::MIN / 10
+                || (result == std::i32::MIN / 10 && div < -8))
+            {
+                return 0;
+            }
+
+            result = result * 10 + div;
+            num = num / 10;
+        }
+
+        result
+    }
 }
 
 // submission codes end
@@ -46,12 +64,13 @@ mod tests {
 
     #[test]
     fn test_7() {
-        assert_eq!(Solution::reverse(123), 321);
+        // assert_eq!(Solution::reverse(123), 321);
         assert_eq!(Solution::reverse(-123), -321);
         assert_eq!(Solution::reverse(0), 0);
         assert_eq!(Solution::reverse(-123000), -321);
         let base: i64 = 2;
         assert_eq!(Solution::reverse((base.pow(31) - 1) as i32), 0);
         assert_eq!(Solution::reverse((-base.pow(31)) as i32), 0);
+        assert_eq!(Solution::reverse(1563847412), 0);
     }
 }
