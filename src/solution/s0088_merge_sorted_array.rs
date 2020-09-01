d93f9866ee5b61b4e719993ca8d07e128b0bee51
crj-gmail <caoruijuan13@gmail.com>
2020-09-01 19:36:38 +0800

    88

--- src/solution/s0088_merge_sorted_array.rs
+++ src/solution/s0088_merge_sorted_array.rs
@@ -32,16 +32,17 @@ impl Solution {
     pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
         let mut i = m - 1;
         let mut j = n - 1;
-        let mut z = m + n - 1;
-        while z >= 0 {
+        let mut k = m + n - 1;
+
+        while k >= 0 {
             if i >= 0 && (j < 0 || nums1[i as usize] >= nums2[j as usize]) {
-                nums1[z as usize] = nums1[i as usize];
+                nums1[k as usize] = nums1[i as usize];
                 i -= 1;
             } else {
-                nums1[z as usize] = nums2[j as usize];
+                nums1[k as usize] = nums2[j as usize];
                 j -= 1;
             }
-            z -= 1;
+            k -= 1;
         }
     }
 }
